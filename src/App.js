1c589f125d427874f29a08402486ec93924ac948
riabikov <mr.android.inc@gmail.com>
2019-04-20 22:09:54 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import connect from '@vkontakte/vkui-connect';
+import { View } from '@vkontakte/vkui';
+import '@vkontakte/vkui/dist/vkui.css';
+
+import Panelname from './panels/Panelname';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			activePanel: 'home',
+			fetchedUser: null,
+		};
+	}
+
+	componentDidMount() {
+		connect.subscribe((e) => {
+			switch (e.detail.type) {
+				case 'VKWebAppGetUserInfoResult':
+					this.setState({ fetchedUser: e.detail.data });
+					break;
+				default:
+					console.log(e.detail.type);
+			}
+		});
+		connect.send('VKWebAppGetUserInfo', {});
+	}
+
+	go = (e) => {
+		this.setState({ activePanel: e.currentTarget.dataset.to })
+	};
+
+	render() {
+		return (
+			<View activePanel={this.state.activePanel}>
+				<Panelname id="home" fetchedUser={this.state.fetchedUser} go={this.go} />
+			</View>
+		);
+	}
+}
+
+export default App;
